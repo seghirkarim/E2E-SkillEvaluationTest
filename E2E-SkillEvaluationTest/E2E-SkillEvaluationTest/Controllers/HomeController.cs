8fa335e27523d770cf43fdde6a2e9e5650c08d0a
Karim Seghir <Seghir.K@hotmail.com>
2020-05-21 10:27:22 +0200

    - Added todoController with CRUD operations - Added Views for CRUD-Operations (Edit, Detail, Add) - Added redirection to error page in case something goes wrong

--- E2E-SkillEvaluationTest/E2E-SkillEvaluationTest/Controllers/HomeController.cs
+++ E2E-SkillEvaluationTest/E2E-SkillEvaluationTest/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using E2E_SkillEvaluationTest.Models;
 using E2E_SkillEvaluationTest.Data;
 using Microsoft.EntityFrameworkCore;
@@ -13,23 +12,16 @@ namespace E2E_SkillEvaluationTest.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly TodoContext _context;
 
-        public HomeController(ILogger<HomeController> logger, TodoContext context)
+        public HomeController(TodoContext context)
         {
-            _logger = logger;
             _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.Todos.ToListAsync());
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ E2E-SkillEvaluationTest/E2E-SkillEvaluationTest/Controllers/TodoController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E2E_SkillEvaluationTest.Data;
+using E2E_SkillEvaluationTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E2E_SkillEvaluationTest.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Todo/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                return View("Details", _context.Todos.ToList().First(i => i.Id == id));
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+        }
+
+        // GET: Todo/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                _context.Todos.Add(new Todo { Title = collection["Title"], Completed = bool.Parse(collection["Completed"].First()) });
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        // GET: Todo/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                return View(_context.Todos.ToList().First(i => i.Id == id));
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }       
+        }
+
+        // POST: Todo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                Todo entity = _context.Todos.Where(t => t.Id == id).FirstOrDefault();
+
+                entity.Title = collection["Title"];
+                entity.Completed = bool.Parse(collection["Completed"].First());
+                _context.SaveChanges();
+
+                return Details(id);
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        // GET: Todo/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Todo entity = _context.Todos.Where(t => t.Id == id).FirstOrDefault();
+
+                _context.Todos.Remove(entity);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+    }
+}
